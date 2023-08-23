aac8a6de6774ce2f6abf0c771c9b4cbebd5a4849
p2103270@etu.univ-lyon1.fr <p2103270@etu.univ-lyon1.fr>
2023-08-23 10:56:28 +0200

    refont de la partie add_a_mektaba.dart

--- lib/pages/mektaba_owner/member/add_a_mektaba.dart
+++ lib/pages/mektaba_owner/member/add_a_mektaba.dart
@@ -10,9 +10,25 @@ class AddMektaba extends StatefulWidget {
   State<AddMektaba> createState() => _AddMektabaState();
 }
 
-class _AddMektabaState extends State<AddMektaba> {
+class _AddMektabaState extends State<AddMektaba>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(vsync: this, length: 2);
+  }
+
   bool checkbool = true;
   bool statut = true;
+  bool autorisations = true;
   String SwitchExpression = "Activé";
   @override
   Widget build(BuildContext context) {
@@ -35,143 +51,205 @@ class _AddMektabaState extends State<AddMektaba> {
       body: Column(
         children: [
           //PARTIE BLACK WHITE
-          Row(
+          Column(
             children: [
-              Expanded(
-                child: Card_Black_White(
-                  label: "Information générales",
-                  backgroundcolor: Colors.black,
-                ),
-              ),
-              Expanded(
-                child: Card_Black_White(
-                  label: "Autorisations",
-                  backgroundcolor: Colors.white,
-                ),
-              ),
-            ],
-          ),
-          //PARTIE FORMULAIRE
-          Padding(
-            padding: EdgeInsets.all(30.0),
-            child: Form(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 20,
+              TabBar(
+                  indicatorColor: Colors.black,
+                  dividerColor: Colors.black,
+                  controller: _tabController,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  tabs: [
+                    Tab(
+                      child: Card_Black_White(
+                        label: "Informations générales",
                       ),
-                      Expanded(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            hintText: "Mosquée Nour",
+                    ),
+                    Tab(child: Card_Black_White(label: "Autorisations"))
+                  ]),
+              Container(
+                height: MediaQuery.of(context).size.height / 1.25,
+                // padding: const EdgeInsets.all(8),
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    //FORMULAIRE
+                    // 2) TODO: RECOLTER LES INFORMATIONS DU QUESTIONNAIRES POUR LA PAGE SUIVANTE(INDICATION AWAIT ASYNC)
+                    SingleChildScrollView(
+                      child: Padding(
+                        padding: EdgeInsets.all(30.0),
+                        child: Form(
+                          child: Column(
+                            children: [
+                              //PHOTO DE L'INSTITUT + NOM DE L'INSTITUT
+                              Row(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 20,
+                                  ),
+                                  Expanded(
+                                    child: TextFormField(
+                                      decoration: const InputDecoration(
+                                        hintText: "Mosquée Nour",
+                                      ),
+                                      style: TextStyle(fontSize: 20.0),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 25.0,
+                              ),
+                              //PARTIE DU SWITCH DU STATUT
+                              Row(
+                                children: [
+                                  Text(
+                                    'Statut',
+                                    style: TextStyle(fontSize: 17.0),
+                                  ),
+                                  SizedBox(
+                                    width: 15.0,
+                                  ),
+                                  Switch(
+                                    activeTrackColor: Colors.lightGreen,
+                                    value: statut,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        statut = value;
+                                        if (value == false) {
+                                          SwitchExpression = "Désactivé";
+                                        } else {
+                                          SwitchExpression = "Activé";
+                                        }
+                                      });
+                                    },
+                                  ),
+                                  SizedBox(
+                                    width: 5.0,
+                                  ),
+                                  Text(SwitchExpression,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey,
+                                        fontSize: 17.0,
+                                      )),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              //RESTE DU FORMULAIRE AVEC LES INFORMATION IMPORTANTE DE L'ENSEIGNE
+                              TextFormField(
+                                decoration:
+                                    const InputDecoration(hintText: "Adresse"),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText: "Code Postale"),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              TextFormField(
+                                decoration:
+                                    const InputDecoration(hintText: "Pays"),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText:
+                                        "Type d'asso(Mosquée,Librairie,etc...)"),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              //Partie avec le Check des Gestions externes
+                              Row(
+                                children: [
+                                  Checkbox(
+                                    value: checkbool,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        checkbool = value!;
+                                      });
+                                    },
+                                  ),
+                                  Text('Gestion externe des numéros de livre'),
+                                ],
+                              ),
+                              //SUITE DU FORMULAIRE AVEC LES INFORMATIONS DU RESPONSABLES
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText: "Nom responsable"),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText: "Mail responsable"),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText: "Numero responsable"),
+                              ),
+                            ],
                           ),
-                          style: TextStyle(fontSize: 20.0),
                         ),
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25.0,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'Statut',
-                        style: TextStyle(fontSize: 17.0),
+                    ),
+                    //Autorisation
+                    SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Row(
+                          children: [
+                            //image
+                            // 1)TODO: CHANGER LES INFORMATIONS POUR PRENDRE CEUX DE LA BASE DE DONNEES
+                            Image.asset(
+                              'assets/pictures/logo_tawhid.png',
+                              height: 50.0,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            //Partie Texte
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Amir BENZIANE',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                Text(
+                                  'Responsable d\' un chat',
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                      SizedBox(
-                        width: 15.0,
-                      ),
-                      Switch(
-                        activeTrackColor: Colors.lightGreen,
-                        value: statut,
-                        onChanged: (value) {
-                          setState(() {
-                            statut = value;
-                            if (value == false) {
-                              SwitchExpression = "Désactivé";
-                            } else {
-                              SwitchExpression = "Activé";
-                            }
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        width: 5.0,
-                      ),
-                      Text(SwitchExpression,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey,
-                            fontSize: 17.0,
-                          )),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(hintText: "Adresse"),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(hintText: "Code Postale"),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(hintText: "Pays"),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: "Type d'asso(Mosquée,Librairie,etc...)"),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Row(
-                    children: [
-                      Checkbox(
-                        value: checkbool,
-                        onChanged: (value) {
-                          setState(() {
-                            checkbool = value!;
-                          });
-                        },
-                      ),
-                      Text('Gestion externe des numéros de livre'),
-                    ],
-                  ),
-                  TextFormField(
-                    decoration:
-                        const InputDecoration(hintText: "Nom responsable"),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  TextFormField(
-                    decoration:
-                        const InputDecoration(hintText: "Mail responsable"),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  TextFormField(
-                    decoration:
-                        const InputDecoration(hintText: "Numero responsable"),
-                  ),
-                ],
-              ),
-            ),
-          )
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
@@ -181,32 +259,21 @@ class _AddMektabaState extends State<AddMektaba> {
 class Card_Black_White extends StatelessWidget {
   Card_Black_White({
     required this.label,
-    required this.backgroundcolor,
   });
   String label;
-  Color backgroundcolor;
+  Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        alignment: Alignment.center,
-        width: double.infinity / 2,
-        height: 50.0,
-        child: Center(
-          child: Text(
-            label,
-            style: TextStyle(
-                fontFamily: "Berlin",
-                color: backgroundcolor == Colors.white
-                    ? Colors.black
-                    : Colors.white,
-                fontSize: 17.0),
-          ),
+    return Container(
+      alignment: Alignment.center,
+      child: Center(
+        child: Text(
+          label,
+          style: TextStyle(fontFamily: "Berlin", fontSize: 17.0),
         ),
-        decoration: BoxDecoration(color: backgroundcolor, border: Border.all()),
       ),
+      //decoration: BoxDecoration(border: Border.all()),
     );
   }
 }
